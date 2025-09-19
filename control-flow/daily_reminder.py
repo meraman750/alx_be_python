c5ae1f28021b5ca45d104e6d7b3fa066c462b5fc
meraman750 <meraman675@gmail.com>
2025-09-19 23:24:29 +0300

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -14,5 +14,5 @@ if time_bound == "yes":
     reminder += " that requires immediate attention today!"
 else:
     reminder += ". Consider completing it when you have free time."
-print('\nReminder:', reminder)
+print('Reminder:', reminder)
 
