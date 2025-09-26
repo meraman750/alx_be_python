9b6f395770a4c53c7ee70d0a312b39f80032da69
Meheretabe Abayneh <meraman675@gmail.com>
2025-09-26 23:26:17 +0300

    Arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+def perform_operation(num1:float, num2:float, operation:str):
+    match operation:
+        case 'add':
+            result = num1 + num2
+        case 'subtract':
+            result = num1 - num2
+        case 'multiply':
+            result = num1 * num2
+        case 'divide':
+            if num2 == 0:
+                return "Error: Cannot divide by zero."
+            else:
+                result = num1 / num2
+        case _:
+            return "Error: Invalid operation"
+    return result
